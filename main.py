1ddac28b7c32c9b8363c58c2e4cd9607d7f75a7b
umpanz <79574205+umpanz@users.noreply.github.com>
2022-12-25 22:01:32 +0100

    Update main.py
    Removed the match case, replaced with a dictionnary with a .get() function

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import webbrowser
 
 iterIndex: int = 1
+categories = {
+    'w' : 'warframes',
+    'p' : 'primaries',
+    's' : 'secondaries',
+    'm' : 'melees'
+}
 
 print('Looking for a (w)arframe, a (p)rimary weapon, a (s)econdary weapon or a (m)elee weapon?')
-category = str(input().lower())
+listItems = categories.get(str(input().lower()),0)
 
-match category:
-    case 'w':
-        listItems = 'warframes'
-    case 'p':
-        listItems = 'primaries'
-    case 's':
-        listItems = 'secondaries'
-    case 'm':
-        listItems = 'melees'
-    case _:
-        print('Not defined (yet)!')
-        input("Press Enter to continue...")
+if listItems == 0:
+    print('Not defined (yet)!')
+    input("Press Enter to continue...")
+else:
+    with open('resources/' + listItems + '.txt') as file:
+        items = [line.rstrip() for line in file]
 
-with open('resources/' + listItems + '.txt') as file:
-    items = [line.rstrip() for line in file]
+    print("Which starting letter?")
+    inputStart = str(input().lower())
+    res = [idx for idx in items if idx[0].lower() == inputStart]
+    for x in res:
+        print(str(iterIndex) + ": " + x)
+        iterIndex = iterIndex + 1
 
-print("Which starting letter?")
-inputStart = str(input().lower())
-res = [idx for idx in items if idx[0].lower() == inputStart.lower()]
-for x in res:
-    print(str(iterIndex) + ": " + x)
-    iterIndex = iterIndex + 1
-
-print("Which item do you want to display?")
-index = str(input())
-webbrowser.open('https://warframe.fandom.com/wiki/' + res[int(index) - 1])
+    print("Which item do you want to display?")
+    index = str(input())
+    webbrowser.open('https://warframe.fandom.com/wiki/' + res[int(index) - 1])
